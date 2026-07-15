408550bec4c1e3569a084fb21917a495130c73fb
Thambi <piravienfive@gmail.com>
2026-07-15 14:22:08 +0100

    testing productList endpoint

--- src/test/java/com/sparta/endpointtesting/ServerUpTest.java
+++ src/test/java/com/sparta/endpointtesting/ServerUpTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class ServerUpTest {
     private static Response response;
 
@@ -27,7 +30,30 @@ public class ServerUpTest {
     @Test
     @DisplayName("Status code 200 returned")
     void testStatusCode200(){
+
+        System.out.println(response.jsonPath().getList("products"));
         MatcherAssert.assertThat(response.statusCode(), Matchers.is(200));
     }
 
-}
+    @Test
+    @DisplayName("When requesting all products and list is returned with all of them.")
+    void testAllProductsReturned(){
+        System.out.println(response.jsonPath().getList("products"));
+        MatcherAssert.assertThat(response.jsonPath().getList("products").size(), Matchers.is(34));
+    }
+
+    @Test
+    @DisplayName("Verify each product contains expected fields")
+    void testAllProductsContainExpectedFields() {
+
+        response.jsonPath().<Map<String, Object>>getList("products")
+                .forEach(product -> {
+                    MatcherAssert.assertThat(product, Matchers.hasKey("id"));
+                    MatcherAssert.assertThat(product, Matchers.hasKey("name"));
+                    MatcherAssert.assertThat(product, Matchers.hasKey("price"));
+                    MatcherAssert.assertThat(product, Matchers.hasKey("brand"));
+                    MatcherAssert.assertThat(product, Matchers.hasKey("category"));
+                });
+    }
+    }
+
---
+++ src/test/java/com/sparta/endpointtesting/pojoconfig/ProductListPojoTest.java
@@ -0,0 +1,5 @@
+package com.sparta.endpointtesting.pojoconfig;
+
+public class ProductListPojoTest {
+
+}
